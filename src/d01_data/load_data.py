630326f31355469426e1c4c7a0fd98caa857b0d3
Kevinb-a <kevin.billet.pro@gmail.com>
2021-03-16 12:25:31 +0100

    Inconsistent Data Entry

--- src/d01_data/load_data.py
+++ src/d01_data/load_data.py
@@ -1,9 +1,14 @@
 
 import pandas as pd
-from src.d00_utils.mysql_utils import mysql_connect, save_to_mysql
+
+import sqlalchemy
+import mysql.connector
+from src.d00_utils.mysql_utils import mysql_connect , save_to_mysql
 
 # After dowloading my csv and xlsx files, I read them with pandas
-df1 = pd.read_excel(r"/home/apprenant/PycharmProjects/American_dream/Data/01_raw/2020_Data_Professional_Salary_Survey_Responses.xlsx", skiprows=3)
+# skprows  allows to start from a certain line 
+df1 = pd.read_excel(r"/home/apprenant/PycharmProjects/American_dream/Data/01_raw/2020_Data_Professional_Salary_Survey_Responses.xlsx", skiprows=3) 
+
 dfk = pd.read_csv(r"/home/apprenant/PycharmProjects/American_dream/Data/01_raw/DataAnalyst.csv")
 
 #Create connection with mysqm
